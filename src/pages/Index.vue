5ed80ccd4fb68fe3be5fe68ecc387f06b585489f
ILadyLuckI <dai.koblitz@gmail.com>
2024-07-22 21:44:13 -0300

    commit inicial

---
+++ src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page padding>
+    <input type="text" 
+      v-model="message" 
+      @keyup.esc="clearMessage" 
+      @keyup.enter="alertMessage"
+    />
+    <button @click="clearMessage">Clear</button>
+    <h5 class="border">{{ message }}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'I love programing and my cats!'
+    }
+  },
+  methods:{
+    clearMessage () {
+      this.message = ''
+    },
+    alertMessage() {
+      alert(this.message)
+    }
+  }
+}
+</script>
