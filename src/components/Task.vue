fb150f9ac32e063d20f8b1c4ba3e38e4c6e5be21
ILadyLuckI <dai.koblitz@gmail.com>
2024-08-22 22:28:10 -0300

    refactor: creating child components

---
+++ src/components/Task.vue
@@ -0,0 +1,15 @@
+<template>
+    <li>
+        <div>{{ task.name }} {{ index }}</div>
+        <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+        <button @click="deleteTask(index)">X</button>
+      </li>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,86 +1,45 @@
 <template>
   <q-page padding>
-    <button style="position: absolute; right: 10px;"
-    @click="counter++">
-      {{ counter }}
-    </button>
-    <input 
-      v-model="message" 
-      v-autofocus
-      @keyup.esc="clearMessage" 
-      @keyup.enter="alertMessage"
-      :style="errorStyle"
-    />
-
-    <button @click="clearMessage">Clear</button>
-
-    <div>{{ message.length }}</div>
-
-    <h5 v-if="message.length" class="border-grey">{{ message }}</h5>
-    <h6 v-else>No message entered 🥱</h6>
-
-    <hr>
-
-    <p>Uppercase message: {{ messageUppercase }}</p>
-    <p>Lowercase message: {{ message | messageLowercase }}</p>
+    <ul>
+      
+    </ul>
   </q-page>
 
 </template>
 
 <script>
+import Task from "../components/Task.vue"
+
 export default {
   data() {
-    return {
-      message: 'I love programing and my cats! <3',
-      counter: 0
+    return{
+      tasks: [
+        {
+          name: 'Go to shop',
+          dueDate: '24/08/2024',
+          dueTime: '18:30'
+        },
+        {
+          name: 'Get bananas',
+          dueDate: '26/08/2024',
+          dueTime: '18:30'
+        },
+        {
+          name: 'Get apples',
+          dueDate: '26/08/2024',
+          dueTime: '18:30'
+        },
+      ]
     }
   },
-  computed: {
-    messageUppercase() {
-      console.log('messageUppercase was fired');
-      return this.message.toUpperCase()
-    },
-    errorStyle() {
-      if(this.message.length > 22)
-      return {
-    'color' : 'red',
-    'background' : 'pink'
-  }
-    }
-  },  
-  methods:{
-    clearMessage () {
-      this.message = ''
-    },
-    alertMessage() {
-      alert(this.message)
-    }
-  },
-  filters: {
-    messageLowercase(value) {
-      return value.toLowerCase()
-    }
-  },
-  directives: {
-    autofocus: {
-      inserted(el) {
-        console.log('input inserted');
-        el.focus()
-      }
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
     }
   }
 }
 </script>
 
 <style>
-  .border-grey {
-    border: 1px solid grey;
-  }
-  input, button {
-    font-size: 23px;
-  }
-  .error {
-    color: red;
-    background: pink;
-  }
+ 
 </style>
