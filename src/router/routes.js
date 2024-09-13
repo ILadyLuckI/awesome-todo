5e29201d7b6536ea1c5f02e5090d87fb678790df
ILadyLuckI <dai.koblitz@gmail.com>
2024-09-12 22:41:23 -0300

    feat: adding navigation scheme to the page

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,25 @@
 const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
-  },
+    {
+        path: '/',
+        component: () => import('layouts/Layout.vue'),
+        children: [
+        { 
+            path: '', 
+            component: () => import('pages/TodoPage.vue') 
+        },
+        { 
+            path: '/settings', 
+            component: () => import('pages/SettingsPage.vue') 
+        }
+        ]
+    },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  }
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+        path: '*',
+        component: () => import('pages/Error404.vue')
+    }
 ]
 
 export default routes
